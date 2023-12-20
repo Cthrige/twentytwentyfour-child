36842b315d9663b10dab843ee624028bfe7e2b13
Cthrige <crillercraller@gmail.com>
2023-12-20 16:43:03 +0100

    functions php child theme changes

--- functions.php
+++ functions.php
@@ -2,8 +2,15 @@
 
 // enqueue parent styles
 
-function ns_enqueue_styles() {
+function child_theme_enqueue_styles() {
+    // Enqueue parent theme styles
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+
+    // Enqueue child theme styles
+    wp_enqueue_style( 'child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( 'parent-style' )
+    );
 }
 
-add_action( 'wp_enqueue_scripts', 'ns_enqueue_styles' );
+add_action( 'wp_enqueue_scripts', 'child_theme_enqueue_styles' );
